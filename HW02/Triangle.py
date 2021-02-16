aee96aeaf0017b9c6826852bfbaf09019fc59745
patelamisha <41029485+patelamisha@users.noreply.github.com>
2021-02-16 12:45:16 -0500

    fixed

--- HW02/Triangle.py
+++ HW02/Triangle.py
@@ -2,13 +2,11 @@
     Date : 02/16/2021
     Assignment : Classify Triagnle"""
 def classifyTriangle(a,b,c):
-    # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'       
     if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'   
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    """It is verify that all 3 inputs are values """
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
         return 'InvalidInput'
     if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
